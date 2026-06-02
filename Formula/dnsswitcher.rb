fb77f8978cd6415b7a0259ac75c54df02fce2923
kacigaya <163902005+kacigaya@users.noreply.github.com>
2026-06-02 12:21:17 +0200

    chore(release): bump to 1.3.0

--- Formula/dnsswitcher.rb
+++ Formula/dnsswitcher.rb
@@ -1,6 +1,6 @@
 cask "dnsswitcher" do
-  version "1.2.2"
-  sha256 "3f55eac3f3711f8c3987e7589145e96ab55e4ca6ce68d746dae0831ca7dadc79"
+  version "1.3.0"
+  sha256 "1e2c0e4e63578f692dfcb15aeb35b243c91c101f732ec8ae36bd61802c2c986e"
 
   url "https://github.com/kacigaya/dns-switcher/releases/download/v#{version}/DNSSwitcher.dmg"
   name "DNS Switcher"
